20926f5284758ea70b23991e240a377f4d4808dc
Basit Oketunde <basitoketunde@basits-mbp.home>
2021-11-29 14:50:42 +0000

    removed extra files

--- main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// nav toggle - select button and links
-var navToggle = document.querySelector("#navToggle");
-var nav = document.querySelector("#nav-links"); // add event listener
-
-navToggle.addEventListener("click", function () {
-  nav.classList.toggle('nav-open');
-});
